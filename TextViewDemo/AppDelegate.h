86f6343b3aca8d57351f6b6912cf05cbc5a1ad61
jingqinglin <jingqinglin@unfae.com>
2016-07-27 14:57:20 +0800

    Initial Commit

---
+++ TextViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextViewDemo
+//
+//  Created by 井庆林 on 16/7/27.
+//  Copyright © 2016年 JingQL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextViewDemo
+//
+//  Created by 井庆林 on 16/7/27.
+//  Copyright © 2016年 JingQL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
